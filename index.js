57ecb13dbd310619c5201eecefaaaa6826cfcdd8
AKHTER KHAN <aktheleo2009@gmail.com>
2024-04-24 08:21:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Import Inquirer module which is a command line interface for Node.js
+import inquirer from "inquirer";
+//Decalre a constant 'answers' and assign it to the result of inqurer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Please enter your sentence to count the words:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//Print the array of words to the consle
+console.log(words);
+//Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
